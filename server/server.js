49446e99793cb65220a357742e40ca757306889a
Rahul Kumar Pandey <rkpandey463@gmail.com>
2026-02-19 18:18:15 +0530

    final-11-commit

--- server/server.js
+++ server/server.js
@@ -13,6 +13,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+  db();
+});
 
 //Routes
 
@@ -29,8 +34,4 @@ app.get("/webhooks", clerkWebhooks);
 
 Sentry.setupExpressErrorHandler(app);
 
-const port = process.env.PORT || 5000;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-  db();
-});
+
